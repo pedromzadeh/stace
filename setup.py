d65aaf1a085c8cae938fb3e20ae341abdc7d6d4a
OakleyNode <oforan@yahoo.com>
2023-05-29 17:12:47 -0400

    Fixed bug

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
    name='stace',
    version='1.0',
    description='Statistical module for cbwins',
-   packages=['stace'],  #same as name
    install_requires=['numpy', 'pandas', 'plotly', 'matplotlib'], #external packages as dependencies
 )
