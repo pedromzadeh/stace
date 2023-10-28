37421df023e0d5a96d2976352531ef8153ebeafc
OakleyNode <oforan@yahoo.com>
2023-10-28 17:00:17 -0400

    Upgraded dataloader

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-   name='stace',
-   version='1.0',
-   description='Statistical module for cbwins',
-   install_requires=['numpy', 'pandas', 'plotly', 'matplotlib'],
-   packages=["stace"]
+    name="stace",
+    version="1.0",
+    description="Statistical module for cbwins",
+    install_requires=["numpy", "pandas", "plotly", "matplotlib"],
+    packages=["stace"],
 )
